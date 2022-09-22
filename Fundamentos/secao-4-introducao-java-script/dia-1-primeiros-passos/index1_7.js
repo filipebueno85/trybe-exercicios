4c0064d0d53541ea28d98e5aba8b4ca99dc56971
Filipe Bueno <filipe_bueno85@live.com>
2022-09-22 09:38:52 -0300

    exercicio 1_7

---
+++ Fundamentos/secao-4-introducao-java-script/dia-1-primeiros-passos/index1_7.js
@@ -0,0 +1,24 @@
+const num1 = 4;
+const num2 = 6;
+const num3 = 8;
+
+if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0)) {
+    console.log(true);
+};
+
+// const a = 1;
+// const b = 3;
+// const c = 5;
+
+// let isOdd = false;
+
+// if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+//   isOdd = true;
+// };
+// console.log(isOdd);
