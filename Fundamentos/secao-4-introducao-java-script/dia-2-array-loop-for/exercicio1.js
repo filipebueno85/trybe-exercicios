5959c1a0bc9c5410e2cde8cb08e3e7da73209342
Filipe Bueno <filipe_bueno85@live.com>
2022-09-22 18:37:00 -0300

    fazendo a media dos numeros somados do array

--- Fundamentos/secao-4-introducao-java-script/dia-2-array-loop-for/exercicio1.js
+++ Fundamentos/secao-4-introducao-java-script/dia-2-array-loop-for/exercicio1.js
@@ -19,15 +19,33 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-}
+// for (index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+// let soma = 0;
+
+// for (index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+// }
+
+// console.log('O resultado da soma é: ' + soma);
 
 let soma = 0;
 
-for (index = 0; index < numbers.length; index += 1) {
+// for (let numero of numbers) {
+//     soma += numero;
+// }
+
+// media =  soma / numbers.length;
+
+// console.log(media);
+
+for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index];
-}
-
-console.log('O resultado da soma é: ' + soma);
-
+  }
+  
+  let average = soma / numbers.length;
+  
+  console.log(average);
+  
