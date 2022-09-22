5c75900a6510cd9b2db79026244bede84e54bbbe
Filipe Bueno <filipe_bueno85@live.com>
2022-09-22 09:20:43 -0300

    exercicio index 1_6

---
+++ Fundamentos/secao-4-introducao-java-script/dia-1-primeiros-passos/index1_6.js
@@ -0,0 +1,11 @@
+const num1 = 4;
+const num2 = 5;
+const num3 = 3;
+
+let isEven = false;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0)) {
+    isEven = true;
+};
+
+console.log(isEven);
