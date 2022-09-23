e8e6784aec5563255a60ffcce4fc6df84470aec7
Filipe Bueno <filipe_bueno85@live.com>
2022-09-23 10:59:28 -0300

    atualizando exercicio

--- Fundamentos/secao-4-introducao-java-script/dia-2-array-loop-for/exercicio1.js
+++ Fundamentos/secao-4-introducao-java-script/dia-2-array-loop-for/exercicio1.js
@@ -55,5 +55,15 @@ for (let index = 0; index < numbers.length; index += 1) {
 
   console.log(media);
 
+//   let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma = soma + numbers[index];
+// };
+
+// console.log('Soma: ', soma);
+// console.log('Média aritimática: ', soma/numbers.length);
+
 
   
