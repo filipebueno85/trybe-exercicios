205a28bfaac400b259b16e752d7635e4abfb0d04
Filipe Bueno <filipe_bueno85@live.com>
2022-09-26 18:20:39 -0300

    exercicios de funcao

---
+++ Fundamentos/secao-4-introducao-java-script/dia-4-javascript-objetos-e-funcoes/testefuncao.js
@@ -0,0 +1,28 @@
+const a = 51;
+const b = 28;
+
+function soma (a, b) {
+    return a + b;
+}
+
+function sub (a, b) {
+    return a - b;
+}
+
+function multiplicacao (a, b) {
+    return a * b;
+}
+
+function div (a, b) {
+    return a / b;
+}
+
+function modulo (a, b) {
+    return a % b;
+}
+
+console.log(soma(a, b));
+console.log(sub(a, b));
+console.log(multiplicacao(a, b));
+console.log(div(a, b));
+console.log(modulo(a, b));
