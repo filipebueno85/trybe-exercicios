05ed9d0d70ce8ceb147a13d10d393d5b1bc3c776
Filipe Bueno <filipe_bueno85@live.com>
2022-10-14 11:46:05 -0300

    exercicio mentoria

---
+++ Fundamentos/secao-6-HTML-e-CSS-rms-Flexbox-e-Responsivo/dia-5-css-responsivo-mobile-first/mentoria.js
@@ -0,0 +1,21 @@
+const botao = document.getElementById("botao");
+const nome = document.getElementById("first-name");
+const sobrenome = document.getElementById("last-name");
+const radio = document.getElementsByClassName("estilos");
+// botao.addEventListener("click", enviarBotao);
+nome.addEventListener('keyup', enviarBotao);
+sobrenome.addEventListener('keyup', enviarBotao);
+
+
+
+
+function enviarBotao() {
+
+  if (nome.value.length > 0 && sobrenome.value.length > 0 && cont === 1) {
+    botao.disabled = false;
+  }
+};
+enviarBotao();
+
+
+
