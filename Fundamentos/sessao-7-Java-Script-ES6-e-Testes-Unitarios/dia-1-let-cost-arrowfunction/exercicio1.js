c5438e550b0de0e30a99bae8a5e79af7de708a5b
Filipe Bueno <filipe_bueno85@live.com>
2022-10-18 11:05:02 -0300

    exercicio

--- Fundamentos/sessao-7-Java-Script-ES6-e-Testes-Unitarios/dia-1-let-cost-arrowfunction/exercicio1.js
+++ Fundamentos/sessao-7-Java-Script-ES6-e-Testes-Unitarios/dia-1-let-cost-arrowfunction/exercicio1.js
@@ -18,3 +18,12 @@ const testingScope = (escopo) => {
   }
 
   testingScope(true);
+
+  // exercício 1
+// verificação && retorno p quando for verdadeiro
+escopo && console.log('DENTRO DO IF');
+
+// vericação ? retorno p quando for verdadeiro : retorno p quando for falso
+const testingScope = (escopo) => escopo ? 'DENTRO DO IF' : 'DENTRO DO ELSE';
+
+console.log(testingScope(false));
