b03b99a06588da41617413e16f06ef449dc75bc2
Filipe Bueno <filipe_bueno85@live.com>
2022-10-19 18:34:58 -0300

    apagando arquivo

--- Fundamentos/sessao-7-Java-Script-ES6-e-Testes-Unitarios/dia-3-primeiros-passos-jest/my_new_project/sum.test.js
+++ Fundamentos/sessao-7-Java-Script-ES6-e-Testes-Unitarios/dia-3-primeiros-passos-jest/my_new_project/sum.test.js
@@ -1,5 +1,13 @@
-const sum = (a, b) => a + b;
+// const sum = (a, b) => a + b;
 
-test('soma dois valores', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
+// test('soma dois valores', () => {
+//   expect(sum(2, 3)).toEqual(5);
+// });
+
+const { sum } =  require('./sum');
+
+describe('Função sum', () => {
+  test('adiciona 4 + 5 igual a 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+};
