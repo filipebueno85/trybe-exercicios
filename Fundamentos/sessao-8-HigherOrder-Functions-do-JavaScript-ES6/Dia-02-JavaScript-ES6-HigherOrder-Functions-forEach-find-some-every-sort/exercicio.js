bebbaa6d58fc214ac6ea372d747220e9b1d90d36
Filipe Bueno <filipe_bueno85@live.com>
2022-10-26 11:40:59 -0300

    exercicios

--- Fundamentos/sessao-8-HigherOrder-Functions-do-JavaScript-ES6/Dia-02-JavaScript-ES6-HigherOrder-Functions-forEach-find-some-every-sort/exercicio.js
+++ Fundamentos/sessao-8-HigherOrder-Functions-do-JavaScript-ES6/Dia-02-JavaScript-ES6-HigherOrder-Functions-forEach-find-some-every-sort/exercicio.js
@@ -1,1 +1,35 @@
-const
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('M', listNames)); // false
+
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = (studentGrades) => (
+  Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+);
+
+console.log(verifyGrades(grades));
---
+++ Fundamentos/sessao-8-HigherOrder-Functions-do-JavaScript-ES6/Dia-02-JavaScript-ES6-HigherOrder-Functions-forEach-find-some-every-sort/parafixar.js
@@ -0,0 +1,83 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// emailListInData.forEach(showEmailList);
+
+// console.log(emailListInData);
+
+// Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => number % 3 === 0 && number % 5 === 0;
+
+const findDivisibleBy = numbers.find(findDivisibleBy3And5);
+
+// console.log(findDivisibleBy);
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+// const findDivisibleBy3And5 = () => {
+//   // Adiciona seu código aqui
+//   return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+// };
+// console.log(findDivisibleBy3And5());
+
+
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = () => {
+//   return names.find((nomes) => nomes.length === 5);
+// };
+
+// console.log(findNameWithFiveLetters());
+
+// Utilize o find para encontrar a música com id igual a 31031685, caso ela exista:
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id);
+};
+
+// console.log(findMusic('31031686'));
+
+// Escreva uma função que recebe dois parâmetros: o primeiro um array de nomes e o segundo, um nome qualquer. Essa função deve retornar true se o segundo parâmetro - um nome qualquer - estiver contido no primeiro parâmetro - array de nomes. Caso não esteja contido, deve retornar false.
+// Dica: use some.
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((currentName) => currentName === name);
+};
+
+// console.log(hasName(names, 'Ana'));
+
+// Escreva uma função, que dado um array de pessoas e uma idade mínima, retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false. Use every;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.
+every((person) => person.age >= minimumAge);
+
+console.log(verifyAges(people, 18)); 
+
