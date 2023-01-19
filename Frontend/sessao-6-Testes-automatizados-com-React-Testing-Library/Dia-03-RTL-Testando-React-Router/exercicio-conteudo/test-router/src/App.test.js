d95083bded99e6cf614a80e587bf3e54750987c4
Filipe Bueno <filipe_bueno85@live.com>
2023-01-19 14:48:39 -0300

    criando sessao 6.3

---
+++ Frontend/sessao-6-Testes-automatizados-com-React-Testing-Library/Dia-03-RTL-Testando-React-Router/exercicio-conteudo/test-router/src/App.test.js
@@ -0,0 +1,73 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App, { About, Home } from './App';
+import renderWithRouter from './renderWithRouter';
+
+// it('deve renderizar o componente App', () => {
+//   renderWithRouter(<App />);
+
+//   const homeTitle = screen.getByRole('heading', {
+//     name: 'Você está na página Início',
+//   });
+//   expect(homeTitle).toBeInTheDocument();
+// });
+describe('teste da aplicação toda', () => {
+it('deve renderizar o componente Sobre', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+  expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+
+  const aboutTitle = screen.getByRole('heading',
+    { name: 'Você está na página Sobre' });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+it('deve renderizar o componente Home', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Início' });
+  expect(homeLink).toBeInTheDocument();
+  userEvent.click(homeLink);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+
+  const homeTitle = screen.getByRole('heading',
+    { name: 'Você está na página Início' });
+  expect(homeTitle).toBeInTheDocument();
+});
+});
+
+it('deve testar um caminho não existente e a renderização do Not Found', () => {
+  const { history } = renderWithRouter(<App />);
+
+  act(() => {
+    history.push('/pagina/que-nao-existe/');
+  })
+
+  const notFoundTitle = screen.getByRole('heading',
+    { name: 'Página não encontrada' });
+  expect(notFoundTitle).toBeInTheDocument();
+});
+
+it('deve renderizar o componente About (apenas componente)', () => {
+  renderWithRouter(<About />);
+
+  const aboutTitle = screen.getByRole('heading',
+    { name: 'Você está na página Sobre' });
+  expect(aboutTitle).toBeInTheDocument();
+});
+
+it('deve renderizar o componente Home (apenas componente)', () => {
+  renderWithRouter(<Home />);
+
+  const homeTitle = screen.getByRole('heading',
+    { name: 'Você está na página Início' });
+  expect(homeTitle).toBeInTheDocument();
+});
