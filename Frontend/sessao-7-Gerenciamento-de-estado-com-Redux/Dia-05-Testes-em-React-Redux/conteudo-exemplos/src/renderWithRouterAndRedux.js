e63e7863613307e0bf1a9a56ff87446e92672119
Filipe Bueno <filipe_bueno85@live.com>
2023-01-31 16:06:06 -0300

    sessao 7.5

---
+++ Frontend/sessao-7-Gerenciamento-de-estado-com-Redux/Dia-05-Testes-em-React-Redux/conteudo-exemplos/src/renderWithRouterAndRedux.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducer } from './redux/reducers/reducer';
+
+const renderWithRouterAndRedux = (
+  component,
+  {
+    initialState = {},
+    store = createStore(reducer, initialState, applyMiddleware(thunk)),
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = {},
+) => ({
+  ...render(
+    <Router history={ history }>
+      <Provider store={ store }>
+        {component}
+      </Provider>
+    </Router>,
+  ),
+  store,
+  history,
+});
---
+++ Frontend/sessao-7-Gerenciamento-de-estado-com-Redux/Dia-05-Testes-em-React-Redux/render-rout-test/src/App.test.js
@@ -0,0 +1,63 @@
+// src/App.test.js
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { legacy_createStore as createStore } from 'redux';
+import App from './App';
+import userReducer from './redux/reducers/userReducer';
+
+const renderWithRouterAndRedux = (
+  component,
+  {
+    initialState = {},
+    store = createStore(userReducer, initialState),
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = {}
+) => ({
+  ...render(
+    <Router history={history}>
+      <Provider store={store}>{component}</Provider>
+    </Router>
+  ),
+  history,
+});
+
+test('Ao acessar a rota /profile a partir da Home, o nome da pessoa deverá ser renderizado', () => {
+  renderWithRouterAndRedux(<App />);
+
+  const nameInput = screen.getByRole('textbox');
+  expect(nameInput).toBeInTheDocument();
+  userEvent.type(nameInput, 'Tryber');
+  expect(nameInput.value).toBe('Tryber');
+
+  const loginButton = screen.getByRole('button');
+  userEvent.click(loginButton);
+
+  expect(screen.getByText('Boas vindas, Tryber')).toBeInTheDocument();
+});
+
+test('Ao acessar diretamente a rota /profile, o nome da pessoa não pode ser renderizado', () => {
+  const initialEntries = ['/profile'];
+
+  renderWithRouterAndRedux(<App />, { initialEntries });
+
+  expect(screen.queryByText('Boas vindas')).not.toBeInTheDocument();
+  expect(screen.getByText('Você precisa realizar o login')).toBeInTheDocument();
+});
+
+test('Ao acessar diretamente a rota /profile e alterando o estado global, o nome da pessoa deve ser renderizado', () => {
+  const initialEntries = ['/profile'];
+  const initialState = { userName: 'Tryber' };
+
+  renderWithRouterAndRedux(<App />, { initialState, initialEntries });
+
+  expect(
+    screen.queryByText('Você precisa realizar o login')
+  ).not.toBeInTheDocument();
+  expect(screen.getByText('Boas vindas, Tryber')).toBeInTheDocument();
+});
