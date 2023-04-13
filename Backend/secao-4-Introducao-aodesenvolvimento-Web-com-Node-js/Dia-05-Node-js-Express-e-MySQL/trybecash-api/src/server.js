7ed46ff1532cd2765c93a2cae0a24a42f9b3e3f5
Filipe Bueno <filipe_bueno85@live.com>
2023-04-13 15:46:09 -0300

    criando sql conection

---
+++ Backend/secao-4-Introducao-aodesenvolvimento-Web-com-Node-js/Dia-05-Node-js-Express-e-MySQL/trybecash-api/src/server.js
@@ -0,0 +1,26 @@
+// src/server.js
+
+const app = require('./app');
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+});
+
+
+// // src/server.js
+// const app = require('./app');
+// const connection = require('./db/connection');
+
+// const PORT = 3001;
+
+// app.listen(PORT, async () => {
+//   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+
+//   // O código abaixo é para testarmos a comunicação com o MySQL
+//   const [result] = await connection.execute('SELECT 1');
+//   if (result) {
+//     console.log('MySQL connection OK');
+//   }
+// });
