148ca826bd57a23ce1ebc67de49d0ef261159178
Filipe Bueno <filipe_bueno85@live.com>
2023-05-23 09:32:28 -0300

    seção 8.2

---
+++ Backend/Secao-8-Introducao-a-TypeScript/Dia-02-Introducao-a-Generics/conteudo/Product.ts
@@ -0,0 +1,14 @@
+export type Product = {
+  barcode: string,
+  price: number
+};
+
+function getProduct(): Product { 
+  const product: Product = {
+   barcode: '123c456b789a', 
+   price: 5,
+  };
+  return product;
+}
+
+console.log(getProduct());
