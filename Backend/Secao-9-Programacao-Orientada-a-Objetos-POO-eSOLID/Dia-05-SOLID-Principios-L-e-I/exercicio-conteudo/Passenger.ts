0bdebcb1c86d590bf75c22cd48aff2765942fe90
Filipe Bueno <filipe_bueno85@live.com>
2023-06-12 09:37:38 -0300

    seção 9.5

---
+++ Backend/Secao-9-Programacao-Orientada-a-Objetos-POO-eSOLID/Dia-05-SOLID-Principios-L-e-I/exercicio-conteudo/Passenger.ts
@@ -0,0 +1,69 @@
+class Passenger {
+  constructor(public nome: string, public cpf: number) { }
+}
+
+class ShippedItem {
+  constructor(
+    public nome: string, 
+    public id: number, 
+    public customerID: string,
+  ) { }
+}
+
+class Flight {
+  constructor(public num: number, public payload: (Passenger | ShippedItem)[]) { }
+
+  add(newPayload: Passenger | ShippedItem): void {
+    this.payload.push(newPayload); 
+  }
+
+
+  remove(removedPassenger: Passenger | ShippedItem): void {
+    const index = this.payload.indexOf(removedPassenger, 0);
+    if (index > -1) {
+      this.payload.splice(index, 1);
+    }
+  }
+}
+
+class Company {
+  constructor(public nome: string, public flights: Flight[]) { }
+  newFlight(flightNum: number): void {
+    const newFlight = new Flight(flightNum, []);
+    this.flights.push(newFlight);
+  }
+  addToFlight(flightNum: number, payload: Passenger | ShippedItem): void { }
+  removeToFlight(flightNum: number, payload: Passenger | ShippedItem): void { }
+}
+
+class TravelingCompany extends Company {
+  addToFlight(flightNum: number, passenger: Passenger): void {
+    const currentFlight = this.flights.find((f) => f.num === flightNum);
+    if (currentFlight) {
+      currentFlight.add(passenger);
+    }
+  }
+
+  removeFromFlight(flightNum: number, passenger: Passenger): void {
+    const currentFlight = this.flights.find((f) => f.num === flightNum);
+    if (currentFlight) {
+      currentFlight.remove(passenger);
+    }
+  }
+}
+
+class ShippingCompany extends Company {
+  addToFlight(flightNum: number, item: ShippedItem): void {
+    const currentFlight = this.flights.find((f) => f.num === flightNum);
+    if (currentFlight) {
+      currentFlight.add(item);
+    }
+  }
+
+  removeToFlight(flightNum: number, item: ShippedItem): void {
+    const currentFlight = this.flights.find((f) => f.num === flightNum);
+    if (currentFlight) {
+      currentFlight.remove(item);
+    }
+  }
+}
